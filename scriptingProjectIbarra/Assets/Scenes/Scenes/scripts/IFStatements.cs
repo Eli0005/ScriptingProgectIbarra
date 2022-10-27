260cc84974e01bb8b298a13993031603c731a666
Eli0005 <116194111+Eli0005@users.noreply.github.com>
2022-10-27 09:09:45 -0700

    added project
    added the scripting project to repo

---
+++ scriptingProjectIbarra/Assets/Scenes/Scenes/scripts/IFStatements.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class IFStatement : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldlimitTemperature = 40.0f;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDowwn(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+
+    void TemperatureTest ()
+    {
+        //if the coffee's temperature is greater than the hottest drinking temperature...
+        if (coffeeTemperature > hotlimitTemperature)
+        {
+            //...do this.
+            print("coffee is too hot");
+        }
+            //If it isn't, but the coffee temperature is less than the coldest drink temperture...
+            else if(coffeeTemperature < coldlimitTemperatureLimit)
---
+++ scriptingProjectIbarra/Assets/Scenes/Scenes/scripts/VariablesAndfunction.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class VariablesAndfunction : MonoBehaviour
+{
+    int myInt = 5;
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo (int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+
+}
